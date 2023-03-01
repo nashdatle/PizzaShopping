c108529bbb340849c76e5f76bd4183b09e54f9a4
Lê Thành Đạt <datdatle2002@gmail.com>
2023-03-01 17:10:13 +0700

    Initial commit

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using PizzaShopping.Models;
+
+namespace PizzaShopping.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(PizzaContext context) { 
+            if (context.Categories.Any())
+            {
+                return;
+            }
+
+            List<Category> categories = new List<Category>
+                {
+                    new Category { CategoryName = "Meat", Description = "For meat eaters" },
+                    new Category { CategoryName = "Vegan", Description = "For meat haters" }
+                };
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using PizzaShopping.Models.Enum;
+
+namespace PizzaShopping.Models
+{
+    public class Account
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AccountId { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public AccountType Type { get; set; }
+    }
+}
